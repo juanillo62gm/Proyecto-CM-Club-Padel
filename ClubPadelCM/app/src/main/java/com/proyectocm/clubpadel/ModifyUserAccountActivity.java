0a60d7fd1b7037224ca891ad7762e9806d5bd6d5
Juanillo62gm <juangomezm97@gmail.com>
2021-05-22 21:34:16 +0200

    Nueva función (Solicitar nuevo correo electrónico)
    En proceso

--- ClubPadelCM/app/src/main/java/com/proyectocm/clubpadel/ModifyUserAccountActivity.java
+++ ClubPadelCM/app/src/main/java/com/proyectocm/clubpadel/ModifyUserAccountActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -31,14 +32,17 @@ public class ModifyUserAccountActivity extends AppCompatActivity {
         final TextView dataName = findViewById(R.id.insertModifiedName);
         final TextView dataSurname = findViewById(R.id.insertModifiedSurname);
         final TextView dataPhone = findViewById(R.id.insertModifiedPhone);
+        final TextView dataEmail = findViewById(R.id.insertModifiedEmail);
 
-        fetchUserData(userId, dataName, dataSurname, dataPhone);
+        fetchUserData(userId, dataName, dataSurname, dataPhone, dataEmail);
 
-        sendDataButton(userId, dataName, dataSurname, dataPhone);
+        buttonSendUserData(userId, dataName, dataSurname, dataPhone);
+
+        buttonSendEmail(dataEmail);
 
     }
 
-    private void fetchUserData(String userId, TextView dataName, TextView dataSurname, TextView dataPhone) {
+    private void fetchUserData(String userId, TextView dataName, TextView dataSurname, TextView dataPhone, TextView dataEmail) {
 
         // Request stored info from DB
         DocumentReference docRef = db.collection("Users").document(userId);
@@ -49,9 +53,12 @@ public class ModifyUserAccountActivity extends AppCompatActivity {
                     String fbName = Objects.requireNonNull(document.get("name")).toString();
                     String fbSurname = Objects.requireNonNull(document.get("surname")).toString();
                     String fbPhone = Objects.requireNonNull(document.get("phone")).toString();
+                    String fbEmail = Objects.requireNonNull(document.get("email")).toString();
                     dataName.setText(fbName);
                     dataSurname.setText(fbSurname);
                     dataPhone.setText(fbPhone);
+                    dataEmail.setText(fbEmail);
+
                 } else {
                     Toast.makeText(getApplicationContext(), "No existe el usuario.", Toast.LENGTH_LONG).show();
                 }
@@ -60,17 +67,6 @@ public class ModifyUserAccountActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-    private void sendDataButton(String userId, TextView dataName, TextView dataSurname, TextView dataPhone) {
-        final Button bEditProfile = findViewById(R.id.buttonStoreNewProfileInfo);
-        bEditProfile.setOnClickListener(v -> {
-            modifyUserData(userId, dataName, dataSurname, dataPhone);
-            Toast.makeText(getApplicationContext(), "Se han modificado los datos del perfil.", Toast.LENGTH_LONG).show();
-            Intent jumpTo = new Intent(ModifyUserAccountActivity.this, MainActivity.class);
-            startActivity(jumpTo);
-            finish();
-        });
     }
 
     private void modifyUserData(String userId, TextView dataName, TextView dataSurname, TextView dataPhone) {
@@ -100,5 +96,43 @@ public class ModifyUserAccountActivity extends AppCompatActivity {
                 .addOnFailureListener(e -> {
                     //Toast.makeText(getApplicationContext(), "Error al actualizar el campo móvil.", Toast.LENGTH_LONG).show();
                 });
+    }
+
+    private void buttonSendUserData(String userId, TextView dataName, TextView dataSurname, TextView dataPhone) {
+        final Button bEditProfile = findViewById(R.id.buttonStoreNewProfileInfo);
+        bEditProfile.setOnClickListener(v -> {
+            modifyUserData(userId, dataName, dataSurname, dataPhone);
+            Toast.makeText(getApplicationContext(), "Se han modificado los datos del perfil.", Toast.LENGTH_LONG).show();
+            Intent jumpTo = new Intent(ModifyUserAccountActivity.this, MainActivity.class);
+            startActivity(jumpTo);
+            finish();
+        });
+    }
+
+    // Hay que iniciar sesión justo antes de modificar este tipo de cosas (WIP)
+    // https://firebase.google.com/docs/auth/android/manage-users#set_a_users_email_address
+    // https://firebase.google.com/docs/auth/android/manage-users#re-authenticate_a_user
+    private void modifyUserEmail(TextView dataEmail) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        user.updateEmail(dataEmail.getText().toString())
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(), "Se ha actualizado el correo correctamente", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Se ha producido un error al actualizar el correo", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void buttonSendEmail(TextView dataEmail) {
+        final Button bEditEmail = findViewById(R.id.buttonRequestEditEmail);
+        bEditEmail.setOnClickListener(v -> {
+            modifyUserEmail(dataEmail);
+            Toast.makeText(getApplicationContext(), "Se ha enviado un email para verificar la nueva dirección de email.", Toast.LENGTH_LONG).show();
+            Intent jumpTo = new Intent(ModifyUserAccountActivity.this, MainActivity.class);
+            startActivity(jumpTo);
+            finish();
+        });
     }
 }
