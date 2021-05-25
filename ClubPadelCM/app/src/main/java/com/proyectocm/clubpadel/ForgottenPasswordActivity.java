6ccf3632facd07fc55c9085aeb272b9a3344f608
Juanillo62gm <juangomezm97@gmail.com>
2021-05-25 04:32:12 +0200

    Ahora se puede restaurar la contraseña olvidada

--- ClubPadelCM/app/src/main/java/com/proyectocm/clubpadel/ForgottenPasswordActivity.java
+++ ClubPadelCM/app/src/main/java/com/proyectocm/clubpadel/ForgottenPasswordActivity.java
@@ -1,16 +1,49 @@
 package com.proyectocm.clubpadel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ForgottenPasswordActivity extends AppCompatActivity {
+
+    // Firebase Authentication
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgotten_password);
-
         setTitle("Recuperar contraseña");
+
+        final TextView dataEmail = findViewById(R.id.insertEmailNewPass);
+
+        buttonChangePassword(dataEmail);
     }
+
+    private void buttonChangePassword(TextView dataEmail) {
+        final Button bChangePassword = findViewById(R.id.buttonRequestNewPass);
+        bChangePassword.setOnClickListener(v -> {
+            requestNewPassword(dataEmail);
+            Intent jumpTo = new Intent(ForgottenPasswordActivity.this, LoginActivity.class);
+            startActivity(jumpTo);
+            finish();
+        });
+    }
+
+    private void requestNewPassword(TextView dataEmail) {
+        mAuth.sendPasswordResetEmail(dataEmail.getText().toString())
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(), "Se ha enviado un correo para que pueda restablecer la contraseña.", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+    }
+
 }
